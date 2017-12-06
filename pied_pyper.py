1e3fff0e9ed0247e29bd4b2c863652ef676e0c96
FleetAdmiral <pranavnair97@gmail.com>
2017-12-06 22:03:46 +0530

    Removed comments

--- pied_pyper.py
+++ pied_pyper.py
@@ -2,14 +2,6 @@ import pip
 def pyp_install(name):
     pip.main(['install', name])
 
-# def impyp(name):
-#     try:
-#         import name
-#         pass
-#     except ModuleNotFoundError as e:
-#         pyp_install(name)
-#         import name
-
 def impyp(name, mods=[]):
     if(mods!=[]):
         try:
